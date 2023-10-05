42e48205607224969e11c02942be956aa855b64b
mariga <mariga.bn>
2023-10-05 13:46:32 -0700

    full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,55 +1,63 @@
 #!/usr/bin/python3
 """
-With Facric , creates a tgz archive
-from web_static content folder
+This fabfile distributes an archive to my web servers
 """
 
-from fabric.api import env, local, put, run
+import os
+from fabric.api import *
 from datetime import datetime
-from os.path import exists, isdir
 
+
+# Set the host IP addresses for web-01 && web-02
 env.hosts = ['34.202.233.215', '54.236.25.198']
-env.user = 'ubuntu'
-env.key_filename = '~/.ssh/id_rsa'
+env.user = "ubuntu"
 
 
 def do_pack():
-    """Creates a tgz archive using fabric"""
-    try:
-        date = datetime.now().strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        filename = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(filename))
-        return filename
-    except Exception as ex:
+    """Create a tar gzipped archive of the directory web_static."""
+    # obtain the current date and time
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    # Construct path where archive will be saved
+    archive_path = "versions/web_static_{}.tgz".format(now)
+
+    # use fabric function to create directory if it doesn't exist
+    local("mkdir -p versions")
+
+    # Use tar command to create a compresses archive
+    archived = local("tar -cvzf {} web_static".format(archive_path))
+
+    # Check archive Creation Status
+    if archived.return_code != 0:
         return None
+    else:
+        return archive_path
 
 
 def do_deploy(archive_path):
-    """deploy web static with fabric"""
-    if exists(archive_path) is False:
-        return False
+    '''use os module to check for valid file path'''
+    if os.path.exists(archive_path):
+        archive = archive_path.split('/')[1]
+        a_path = "/tmp/{}".format(archive)
+        folder = archive.split('.')[0]
+        f_path = "/data/web_static/releases/{}/".format(folder)
 
-    try:
-        filename = archive_path.split("/")[-1]
-        no_excep = filename.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('sudo mkdir -p {}{}/'.format(path, no_excep))
-        run('sudo tar -xzf /tmp/{} -C {}{}/'.format(filename, path, no_excep))
-        run('sudo rm /tmp/{}'.format(filename))
-        run('sudo mv {0}{1}/web_static/* {0}{1}/'.format(path, no_excep))
-        run('sudo rm -rf {}{}/web_static'.format(path, no_excep))
-        run('sudo rm -rf /data/web_static/current')
-        run('sudo ln -s {}{}/ /data/web_static/current'.format(path, no_excep))
+        put(archive_path, a_path)
+        run("mkdir -p {}".format(f_path))
+        run("tar -xzf {} -C {}".format(a_path, f_path))
+        run("rm {}".format(a_path))
+        run("mv -f {}web_static/* {}".format(f_path, f_path))
+        run("rm -rf {}web_static".format(f_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(f_path))
         return True
-    except BaseException:
-        return False
+    return False
 
 
 def deploy():
-    """ do path an do deploy"""
+    """
+    Create and archive and get its path
+    """
     archive_path = do_pack()
     if archive_path is None:
         return False
